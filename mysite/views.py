0289d25ca3da831086d69ea596de71b4dd6541e7
songchiping <11056009@ntub.edu.tw>
2023-10-18 16:22:11 +0800

    做出表格連結加介面

--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from mysite.models import Post
 from django.http import HttpResponse
 from datetime import datetime
+from django.shortcuts import redirect
 # Create your views here.
 def homepage(request):
     posts = Post.objects.all()
@@ -9,8 +10,14 @@ def homepage(request):
     return render(request, 'index.html' , locals())
 
 def showpost(request, slug):
-    post=Post.objects.get(slug=slug)
-    return render(request, 'post.html' , locals())
+    try:
+        post=Post.objects.get(slug=slug)
+        if post != None:            
+            return render(request, 'post.html' , locals())
+        else:
+            return redirect("/") #導到首頁
+    except:
+        return redirect("/")
 
 '''
 def homepage(request):    
